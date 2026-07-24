3599b67ac5602d3f98338044b58be80f796ae5a2
Alvaro Rodriguez <alvarord519@gmail.com>
2026-07-23 22:53:00 -0500

    refactor: relocate clinical history spec

--- src/patients/history/clinical-history.services.spec.ts
+++ src/patients/history/clinical-history.services.spec.ts
@@ -1,15 +1,15 @@
 import { ConflictException } from '@nestjs/common';
 import { Repository } from 'typeorm';
-import { Interaction } from './database/entities/interaction.entity';
-import { PatientDiagnosis } from './patients/entities/patient-diagnosis.entity';
-import { PatientInsurance } from './patients/entities/patient-insurance.entity';
-import { PatientMedicalAppointment } from './patients/entities/patient-medical-appointment.entity';
-import { PatientTreatment } from './patients/entities/patient-treatment.entity';
-import { HistoryVersioningService } from './patients/history-versioning/history-versioning.service';
-import { PatientInsuranceService } from './patients/clinical/insurance/patient-insurance.service';
-import { PatientMedicalAppointmentsService } from './patients/clinical/medical-appointments/patient-medical-appointments.service';
-import { PatientTreatmentsService } from './patients/clinical/treatments/patient-treatments.service';
-import { PatientsService } from './patients/patients.service';
+import { Interaction } from '../../../database/entities/interaction.entity';
+import { PatientDiagnosis } from '../entities/patient-diagnosis.entity';
+import { PatientInsurance } from '../entities/patient-insurance.entity';
+import { PatientMedicalAppointment } from '../entities/patient-medical-appointment.entity';
+import { PatientTreatment } from '../entities/patient-treatment.entity';
+import { HistoryVersioningService } from '../history-versioning/history-versioning.service';
+import { PatientInsuranceService } from '../clinical/insurance/patient-insurance.service';
+import { PatientMedicalAppointmentsService } from '../clinical/medical-appointments/patient-medical-appointments.service';
+import { PatientTreatmentsService } from '../clinical/treatments/patient-treatments.service';
+import { PatientsService } from '../patients.service';
 
 describe('clinical history services', () => {
   const patients = {
