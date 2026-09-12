97f1673521971ad8af3f6512dd10290e6a829189
Alvaro Rodriguez <alvarord519@gmail.com>
2026-09-12 18:17:39 -0500

    fix(documents): correct association constraint

--- src/database/migrations/1784902000000-ExpandPatientDocumentTypes.ts
+++ src/database/migrations/1784902000000-ExpandPatientDocumentTypes.ts
@@ -14,7 +14,7 @@ export class ExpandPatientDocumentTypes1784902000000 implements MigrationInterfa
       `ALTER TABLE "patient_documents" DROP CONSTRAINT "CHK_patient_documents_association"`,
     );
     await queryRunner.query(
-      `ALTER TABLE "patient_documents" ADD CONSTRAINT "CHK_patient_documents_association" CHECK (("document_type" = 'MEDICAL_REPORT' AND "diagnosis_id" IS NOT NULL AND "treatment_id" IS NULL) OR ("document_type" = 'PRESCRIPTION' AND "diagnosis_id" IS NULL AND "treatment_id" IS NOT NULL) OR ("document_type" IN ('CLINICAL_HISTORY', 'APPOINTMENT_SCHEDULE', 'MEDICAL_ORDER', 'EXAM_RESULTS', 'REFERRAL_OR_COUNTERREFERRAL', 'IDENTITY_DOCUMENT', 'CONADIS_DISABILITY_DOCUMENT') AND "diagnosis_id" IS NULL AND "treatment_id" IS NULL) OR ("document_type" = 'OTHER' AND "diagnosis_id" IS NULL AND "treatment_id" IS NULL AND "description" IS NOT NULL AND length(btrim("description")) > 0)))`,
+      `ALTER TABLE "patient_documents" ADD CONSTRAINT "CHK_patient_documents_association" CHECK (("document_type" = 'MEDICAL_REPORT' AND "diagnosis_id" IS NOT NULL AND "treatment_id" IS NULL) OR ("document_type" = 'PRESCRIPTION' AND "diagnosis_id" IS NULL AND "treatment_id" IS NOT NULL) OR ("document_type" IN ('CLINICAL_HISTORY', 'APPOINTMENT_SCHEDULE', 'MEDICAL_ORDER', 'EXAM_RESULTS', 'REFERRAL_OR_COUNTERREFERRAL', 'IDENTITY_DOCUMENT', 'CONADIS_DISABILITY_DOCUMENT') AND "diagnosis_id" IS NULL AND "treatment_id" IS NULL) OR ("document_type" = 'OTHER' AND "diagnosis_id" IS NULL AND "treatment_id" IS NULL AND "description" IS NOT NULL AND length(btrim("description")) > 0))`,
     );
   }
 
